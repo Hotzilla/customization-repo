9c2edbc9bfbb9098ecba72884cc7f6beeacceed3
Hotzilla <tuukka.lindroos@gmail.com>
2017-12-20 13:00:09 +0200

    changed url of css location, and hoping this is where it ends up

--- static/communities/communitiesCustomization.js
+++ static/communities/communitiesCustomization.js
@@ -5,7 +5,7 @@
 //
 if(typeof(dojo) != "undefined") {
     dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/static/communities/communitiesCustomization.css\"></link>",
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/communities/communitiesCustomization.css\"></link>",
         dojo.doc.head,
         "last"
     );
